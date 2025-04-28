76343e653667b69f578ff089cd80a2ec1a9eeaa5
RaghavOG <04raghavsingla28@gmail.com>
2025-04-29 00:38:13 +0530

    Added Shadcn and Theme Provider

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,12 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  images: {
+    domains: ["res.cloudinary.com"],
+  },
+  typescript: {
+    ignoreBuildErrors: true, 
+  }
 };
 
 export default nextConfig;
